1f9411ae9c9411b0a3a6c8bc7330b7758f4eacf1
Libório Júnior <libajunior@gmail.com>
2024-09-23 09:23:45 -0300

    Aula 20/08/2024

--- mater-play-frontend/src/main.tsx
+++ mater-play-frontend/src/main.tsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
+
 import App from './App.tsx'
 
 import '@fontsource/roboto/300.css';
@@ -7,8 +9,21 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#fff'
+    }
+  }
+})
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={ darkTheme }>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
